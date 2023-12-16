89b026482a1715d366dce89ca71c12cb3452f63d
hongshaoli <hongshaoli@foxmail.com>
2023-12-16 11:33:26 +0800

    feat：铭文批量页面开发完成

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,148 @@
-import Image from 'next/image'
+'use client'
 
-export default function Home() {
+import {
+  Box,
+  Button,
+  Input,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  FormControl,
+  FormLabel,
+  Heading,
+  useToast
+} from '@chakra-ui/react'
+import {useEffect, useState} from "react";
+import Web3 from 'web3'
+
+export default function MyApp() {
+  const toast = useToast()
+
+  const [address, setAddress] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [hexData, setHexData] = useState('');
+  const [customGas, setCustomGas] = useState('');
+  const [count, setCount] = useState(1);
+  const [account, setAccount] = useState('');
+  const [web3, setWeb3] = useState(null);
+
+
+  async function init() {
+    const web3Obj = new Web3(window.ethereum);
+    const accounts = await web3Obj.eth.requestAccounts()
+    setAccount(accounts[0])
+    setWeb3(web3Obj)
+  }
+
+  useEffect(() =>{
+    init().then()
+  }, [])
+
+  // 发送交易
+  async function onBatchSend() {
+    if(!window.ethereum) {
+      toast({
+        title: `请安装Metamask插件`,
+        status: 'error',
+        isClosable: true,
+      })
+      return
+    }
+
+    if(!account) {
+      await init()
+      return
+    }
+
+    for (let i = 0; i < count; i++) {
+      sendTransaction().then()
+    }
+  }
+
+  async function sendTransaction() {
+    try {
+      const realGasPrice = await web3.eth.getGasPrice();
+
+      const transactionObject = {
+        from: account,
+        to: address,
+        data: hexData || undefined,
+        gasPrice: customGas ? web3.utils.toWei(customGas, 'gwei') :  realGasPrice,
+        value: web3.utils.toWei(amount.toString(), 'ether')
+      };
+
+      // 发送交易
+      const transactionHash = await web3.eth.sendTransaction(transactionObject);
+      // console.log('交易哈希：', transactionHash);
+      toast({
+        position: 'bottom-right',
+        title: `交易成功~ ${transactionHash.blockHash}`,
+        status: 'success',
+        isClosable: true,
+      })
+    } catch (error) {
+      console.error(error);
+      toast.closeAll()
+      toast({
+        position: 'bottom-right',
+        title: error.message,
+        status: 'error',
+        isClosable: true,
+      })
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box maxW="750px" mx="auto" p={6}>
+      <Heading as='h3' size="lg" textAlign="center" mb={2}> 铭文小工具 </Heading>
+      <FormControl isRequired pt={2}>
+        <FormLabel>转账地址</FormLabel>
+        <Input type='text' placeholder="0x1234..." value={address} onChange={e => setAddress(e.target.value)} />
+      </FormControl>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <FormControl isRequired pt={2}>
+        <FormLabel>转账金额</FormLabel>
+        <NumberInput defaultValue={0} min={0}>
+          <NumberInputField value={amount} onChange={e => setAmount(e.target.value)} placeholder="默认 0" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <FormControl pt={2}>
+        <FormLabel>自定义data</FormLabel>
+        <Input type='text' value={hexData} onChange={e => setHexData(e.target.value)} placeholder="非必填，十六进制数据" />
+      </FormControl>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <FormControl pt={2}>
+        <FormLabel>自定义gas</FormLabel>
+        <NumberInput min={0}>
+          <NumberInputField value={customGas} onChange={e => setCustomGas(e.target.value)} placeholder="非必填" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <FormControl isRequired pt={2}>
+        <FormLabel>调用次数</FormLabel>
+        <NumberInput min={1} defaultValue={1}>
+          <NumberInputField value={count} onChange={e => setCount(e.target.value)} placeholder="默认1次" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </FormControl>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Button mt={4} width="100%" colorScheme='blue' variant='solid' onClick={onBatchSend}>
+        {account ? '发送交易' : '连接钱包'}
+      </Button>
+    </Box>
   )
 }
